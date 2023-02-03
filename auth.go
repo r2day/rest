8fcfaa6d96984240f09e611e0450dac71305bb07
dropthemasquerade <dropthemasquerade@gmail.com>
2023-02-04 00:18:17 +0800

    add auth render

---
+++ auth.go
@@ -0,0 +1,64 @@
+package rest
+
+import (
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	DefaultExpireLoginSessionTime = 24 // 默认 登陆有效时长24h
+)
+
+var (
+	ExpireLoginSessionTime = DefaultExpireLoginSessionTime
+	CustomLoginExpireTime = os.Getenv("CUSTOME_LOGIN_EXPIRE_TIME")
+)
+
+func init() {
+	if CustomLoginExpireTime != "" {
+		ExpireLoginSessionTime, _ = strconv.Atoi(CustomLoginExpireTime)
+	}
+}
+
+// RenderLogin 返回登陆信息
+func RenderLogin(c *gin.Context, accountId string, passwordFromDB []byte, passwordFromReq string, secretKey string, host string) {
+	
+	// 检查密码hash是否相同
+	if err := bcrypt.CompareHashAndPassword(passwordFromDB, []byte(passwordFromReq)); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// 声明密码签名
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    accountId,
+		ExpiresAt: time.Now().Add(time.Hour * time.Duration(ExpireLoginSessionTime)).Unix(), //1 day
+	})
+
+	// 签名密钥
+	token, err := claims.SignedString([]byte(secretKey))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// 设置缓存过期时间
+	c.SetCookie("jwt", token, 3600 * ExpireLoginSessionTime, "/", host, false, false)
+
+	// 
+	c.JSON(http.StatusOK, gin.H{
+		"status":  "ok",
+		"message": "login success",
+		"expire": ExpireLoginSessionTime,
+		"expire_unit": "hours",
+	})
+
+	// TODO 发生登陆消息到mq中记录登陆信息
+}
--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/r2day/rest
 
 go 1.19
 
-require github.com/gin-gonic/gin v1.8.1
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-gonic/gin v1.8.1
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
+)
 
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -17,7 +21,6 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/text v0.3.6 // indirect
